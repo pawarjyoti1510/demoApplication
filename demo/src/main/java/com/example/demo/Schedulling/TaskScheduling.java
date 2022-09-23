5b1f5a6fcf96009133573f37b6623b89e1c94f86
jyoti pawar <pawarjyoti1510@gmail.com>
2022-09-23 14:14:39 +0530

    scheduler jp

--- demo/src/main/java/com/example/demo/Schedulling/TaskScheduling.java
+++ demo/src/main/java/com/example/demo/Schedulling/TaskScheduling.java
@@ -1,20 +1,18 @@
 package com.example.demo.Schedulling;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+
 @Component
 public class TaskScheduling {
 
-    private static final Logger logger = LoggerFactory.getLogger(TaskScheduling.class);
-    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+    Logger logger =  LoggerFactory.getLogger(TaskScheduling.class);
 
-    @Scheduled(fixedRate = 50000)
+    @Scheduled(cron = "0 0/1 * * * *")
     public void reportCurrentTime() {
-        logger.info("The time is now {}", dateFormat.format(new Date()));
+       logger.info("hello");
+
     }
 }
